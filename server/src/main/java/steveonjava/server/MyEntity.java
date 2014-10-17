36bf04c7f93f1c25b25fafec706a4a04736d56ce
stefan.schulz <stefan.schulz@bcc-ws65.bcc.intern>
2014-10-17 15:16:12 +0200

    added test entity class

---
+++ server/src/main/java/steveonjava/server/MyEntity.java
@@ -0,0 +1,28 @@
+package steveonjava.server;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MyEntity implements Serializable {
+
+	private static final long serialVersionUID = 6620905633391064461L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private String someEntityValue;
+    
+    public String getSomeEntityValue() {
+		return someEntityValue;
+	}
+    
+    public void setSomeEntityValue(String someEntityValue) {
+		this.someEntityValue = someEntityValue;
+	}
+}
